e5affafd6377c1aed5124e8f01d08e6267ab7fd2
Nightstars <christzhangowner#gmail.com>
2020-12-20 13:09:55 +0800

    add Wechat message

---
+++ src/christ.Xncf.WechatMessage/Hander/MessageContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace christ.Xncf.WechatMessage.Hander
+{
+    class MessageContext
+    {
+    }
+}
---
+++ src/christ.Xncf.WechatMessage/Hander/MessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using Senparc.NeuChar.Entities;
+using Senparc.Weixin.MP.MessageHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace christ.Xncf.WechatMessage.Hander
+{
+    public class MessageHandler //: MessageHandler<MessageContext>
+    {
+        //public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
